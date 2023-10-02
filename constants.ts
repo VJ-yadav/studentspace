26a330fa1a49f7bbfdcdd06ba3f3a6d972bf0171
VJ-yadav <vjyadav194@gmail.com>
2023-10-02 19:48:09 -0400

    I tried to develop a REACT Application.

---
+++ constants.ts
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const socials = [
+    {
+        id: 1,
+        name: "Twitter",
+        url: "https://twitter.com/vijayya60865443",
+        handle: "@vijayya60865443",
+    },
+    {
+        id: 2,
+        name: "Instagram",
+        url: "https://www.instagram.com/vjsnapp/",
+        handle: "@vjsnapp",
+    },
+];
